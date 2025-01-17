42c47e59346491055a24a720fef4109a8a0d25e3
Jostin Aguilar <aguilarjostin2001@gmail.com>
2025-01-17 18:59:40 -0500

    read data from db

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,39 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Job
+class Job extends Model
 {
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000',
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10,000',
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40,000',
-            ]
-        ];
-    }
+    protected $fillable = ['title', 'salary'];
 
-    public static function find(int $id): array
-    {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
+    protected $table = 'job_listings';
 
-        if (!$job) {
-            abort(404);
-        }
-
-        return $job;
-    }
+    //
 }

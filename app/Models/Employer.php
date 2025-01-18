1a36610d4110216981d0dd720d16fac855323f58
Jostin Aguilar <aguilarjostin2001@gmail.com>
2025-01-17 19:52:41 -0500

    add factories

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -5,14 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Job extends Model
+class Employer extends Model
 {
 
     use HasFactory;
-
-    protected $fillable = ['title', 'salary'];
-
-    protected $table = 'job_listings';
-
     //
 }

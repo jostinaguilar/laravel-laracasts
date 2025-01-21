87faa9a9893303c4cafcf172a81d83fcd9a6c55c
Jostin Aguilar <aguilarjostin2001@gmail.com>
2025-01-20 22:31:35 -0500

    fix routes web

--- resources/views/jobs/edit.blade.php
+++ resources/views/jobs/edit.blade.php
@@ -49,10 +49,11 @@
         <div class="mt-6 flex items-center justify-between gap-x-6">
             <div class="flex items-center">
                 <button form="delete-job"
-                        class="rounded-md border border-red-200 bg-red-50 px-2.5 py-1.5 font-semibold text-red-500">Delete</button>
+                        class="rounded-lg border border-red-700 bg-red-50 px-3 py-2 text-center text-sm font-medium text-red-700 hover:bg-red-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-red-300 dark:border-red-500 dark:text-red-500 dark:hover:bg-red-600 dark:hover:text-white dark:focus:ring-red-900">Delete</button>
             </div>
             <div class="flex items-center gap-x-6">
-                <a href="/jobs/{{ $job->id }}" class="text-sm/6 font-semibold text-gray-900">Cancel</a>
+                <a href="/jobs/{{ $job->id }}"
+                   class="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Cancel</a>
                 <button type="submit"
                         class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Update</button>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -1,91 +1,22 @@
 <?php
 
+use App\Http\Controllers\JobsController;
 use App\Models\Job;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::view('/', 'home');
 
-Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->latest()->simplePaginate(3);
-    return view('jobs.index', ['jobs' => $jobs]);
-});
+/* Route::controller(JobsController::class)->group(function () {
+    Route::get('/jobs', 'index');
+    Route::get('/jobs/create', 'create');
+    Route::post('/jobs', 'store');
+    Route::get('/jobs/{job}', 'show');
+    Route::get('/jobs/{job}/edit', 'edit');
+    Route::patch('/jobs/{job}', 'update');
+    Route::delete('/jobs/{job}', 'destroy');
+}); */
 
-// Create a new job
-Route::get('/jobs/create', function () {
-    return view('jobs.create');
-});
+Route::resource('jobs', JobsController::class);
 
-// Store the new job
-Route::post('/jobs', function () {
-
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required']
-    ]);
-
-    Job::create([
-        'title' => request('title'),
-        'salary' => request('salary'),
-        'employer_id' => 1
-    ]);
-
-    return redirect('/jobs');
-});
-
-// Show a single job
-Route::get('/jobs/{id}', function ($id) {
-    $job = Job::find($id);
-
-    return view('jobs.show', [
-        'job' => $job
-    ]);
-});
-
-// Edit a job
-Route::get('/jobs/{id}/edit', function ($id) {
-    $job = Job::find($id);
-
-    return view('jobs.edit', [
-        'job' => $job
-    ]);
-});
-
-// Udpate a job
-Route::patch('/jobs/{id}', function ($id) {
-
-    // Validate the request
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required']
-    ]);
-
-    // Authorise the request (On Hold...)
-    // Update the job
-    $job = Job::findOrFail($id);
-
-    $job->update([
-        'title' => request('title'),
-        'salary' => request('salary')
-    ]);
-
-    // Redirect to the job page
-    return redirect("/jobs/{$job->id}");
-});
-
-// Destroy a job
-Route::delete('/jobs/{id}', function ($id) {
-    //Authorise the request (On Hold...)
-    // Delete the job
-    $job = Job::findOrFail($id);
-    $job->delete();
-
-    // Redirect to the jobs page
-    return redirect('/jobs');
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::view('/contact', 'contact');
